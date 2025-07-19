e71dd845563af02135f521713f3baa1907e1bd46
Thomas <thomascoxpowell@gmail.com>
2025-07-19 13:28:05 -0400

    starting component construction

---
+++ src/lib/Dropdown.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let options: string[] = [];
+  export let selected: string = '';
+  export let label: string = '';
+</script>
+
+<label>
+  {label}
+  <select bind:value={selected}>
+    {#each options as option}
+      <option value={option}>{option}</option>
+    {/each}
+  </select>
+</label>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  // imports
+  import Dropdown from "../lib/Dropdown.svelte"
+
+  // volume options
+  const volumeOptions = ["0.0", "0.5", "1.0", "1.5", "2"];
+
+  // job data
+	let url = '';
+	let format = 'mp4';
+	let volumeScale = '1.0';
+	let startTime = '';
+	let endTime = '';
+
+  // status info
+	let status = ''; 
+	let jobId = '';
+  
+  async function submit_job() {
+    status = "submitting"
+		const data = {
+			url,
+			format,
+			volumeScale,
+			startTime,
+			endTime
+		};
+    const res = await fetch('http://localhost:8080/videos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });     
+    const json = await res.json(); 
+    if (!res.ok) {
+      status = json.error || "unknown error";
+      return;
+    }
+    status = json.message;
+    jobId = json.id;
+  }
+</script>
+
+
+
+<p>clip</p>
+<button on:click={submit_job}>send job</button>
+<p>volume scale: {volumeScale}</p>
+<Dropdown
+  label="volume scale"
+  options={volumeOptions}
+  bind:selected={volumeScale}
+/>
+
+
