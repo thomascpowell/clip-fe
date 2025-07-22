9228e09e835e8e66b17db9a5ec1f8aee9c4e6c14
Thomas <thomascoxpowell@gmail.com>
2025-07-21 22:53:13 -0400

    i am not a designer

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,27 @@
+<div>
+  <p>[clip]</p>
+  <span>
+    <a href="https://thomascpowell.com">[web]</a>
+    <a href="https://github.com/thomascpowell/clip">[repo]</a>
+  </span>
+</div>
+
+
+<style>
+  div {
+    padding: 1em;
+    width: 100%;
+    height: min-content;
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    top: 0;
+    color: var(--text);
+    left: 0;
+    right: 0;
+  }
+  a {
+    color: var(--text2);
+    text-decoration: none;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import JobForm from "../lib/JobForm.svelte"
+  import Nav from "../lib/Nav.svelte"
 </script>
 
-
+<Nav/>
 <JobForm/>
